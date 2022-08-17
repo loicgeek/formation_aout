178bb6bffd9225e6a0e8ebc1f6c9b78a43a32da4
loicgeek <loic.ngou98@gmail.com>
2022-08-17 12:44:40 +0100

    Tab, defaultTabController, TabBarView, ListView

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class Application extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: const HomeScreen(),
     );
@@ -41,52 +41,318 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("GesStock"),
-        leading: const Icon(Icons.menu),
-        centerTitle: true,
-        elevation: 0,
-        actions: const [
-          Icon(Icons.search),
-          Icon(Icons.more_vert),
-        ],
-      ),
-      body: Container(
-        color: Colors.grey.withOpacity(.2),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Divider(),
-              FacebookCard(),
-              Divider(),
-              FacebookCard(),
-              Divider(),
-              FacebookCard(),
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text(
+            "facebook",
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 25,
+            ),
+          ),
+          centerTitle: false,
+          elevation: 0,
+          actions: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 6),
+              padding: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.withOpacity(.1),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 6),
+              padding: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.withOpacity(.1),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 6),
+              padding: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.withOpacity(.1),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.message,
+                color: Colors.black,
+              ),
+            ),
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.home_outlined,
+                  color: Colors.black,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.group_outlined,
+                  color: Colors.black,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.store_outlined,
+                  color: Colors.black,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.card_giftcard,
+                  color: Colors.black,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.notifications_outlined,
+                  color: Colors.black,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.menu_outlined,
+                  color: Colors.black,
+                ),
+              ),
             ],
           ),
         ),
+        body: TabBarView(
+          children: [
+            UserFeeds(),
+            UserFeeds(),
+            UserFeeds(),
+            UserFeeds(),
+            UserFeeds(),
+            UserFeeds(),
+          ],
+        ),
+        // bottomNavigationBar: BottomNavigationBar(
+        //   items: const [
+        //     BottomNavigationBarItem(
+        //       label: "Home",
+        //       icon: Icon(Icons.home),
+        //     ),
+        //     BottomNavigationBarItem(
+        //       label: "Settings",
+        //       icon: Icon(Icons.settings),
+        //     ),
+        //   ],
+        // ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {},
+        //   child: const Icon(Icons.add),
+        // ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            label: "Home",
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            label: "Settings",
-            icon: Icon(Icons.settings),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class UserFeeds extends StatefulWidget {
+  const UserFeeds({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<UserFeeds> createState() => _UserFeedsState();
+}
+
+class _UserFeedsState extends State<UserFeeds> {
+  late TextEditingController _postController;
+  @override
+  void initState() {
+    _postController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey.withOpacity(.2),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: Container(
+                          width: 15,
+                          height: 15,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.green,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 0.0, left: 4),
+                      child: TextFormField(
+                        autofocus: false,
+                        controller: _postController,
+                        decoration: InputDecoration(
+                          isDense: true,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          hintText: "What's on your mind?",
+                          hintStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                            ),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.photo,
+                      color: Colors.green,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Divider(thickness: .1),
+            Container(
+              height: MediaQuery.of(context).size.height * .32,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: DefaultTabController(
+                length: 2,
+                child: Column(
+                  children: [
+                    TabBar(
+                      labelColor: Colors.black,
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                      tabs: [
+                        Tab(
+                          text: "Stories",
+                        ),
+                        Tab(
+                          text: "Reels",
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: TabBarView(
+                          children: [
+                            ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: [
+                                for (int i = 0; i < 5; i++)
+                                  Container(
+                                    margin: EdgeInsets.only(right: 5),
+                                    decoration: BoxDecoration(
+                                        color: Colors.primaries[i],
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    width: (MediaQuery.of(context).size.width /
+                                            3.3 -
+                                        5 -
+                                        10),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              CircleAvatar(
+                                                backgroundColor: Colors
+                                                    .primaries.reversed
+                                                    .elementAt(i),
+                                              ),
+                                            ],
+                                          ),
+                                          Text(
+                                            "Lucas Filario",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                            ListView(
+                              scrollDirection: Axis.horizontal,
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Divider(),
+            FacebookCard(),
+            Divider(),
+            FacebookCard(),
+            Divider(),
+            FacebookCard(),
+          ],
+        ),
       ),
     );
   }
